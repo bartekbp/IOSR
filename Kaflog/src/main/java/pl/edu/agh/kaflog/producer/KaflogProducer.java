4c420a659c14228078625f71be874c57005ee2d4
Łukasz Opioła <lukasz.opiola@gmail.com>
2014-04-30 14:02:02 +0200

    Add new files

---
+++ Kaflog/src/main/java/pl/edu/agh/kaflog/producer/KaflogProducer.java
@@ -0,0 +1,87 @@
+package pl.edu.agh.kaflog.producer;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+import java.io.IOException;
+import java.net.*;
+import java.text.ParseException;
+import java.util.Properties;
+
+public class KaflogProducer {
+    private Producer<Integer, String> producer;
+    private Properties props = new Properties();
+
+
+    private final TimeStatistics lastMinute = new TimeStatistics(1, 60);
+    private final TimeStatistics lastHour = new TimeStatistics(60, 60);
+    private final TimeStatistics lastDay = new TimeStatistics(3600, 24);
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Current time: " + TimeStatistics.getCurrentDate());
+            new KaflogProducer();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public KaflogProducer() throws Exception {
+        props.put("metadata.broker.list", "localhost:9092");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("request.required.acks", "1");
+        producer = new Producer<>(new ProducerConfig(props));
+        tentego();
+        producer.close();
+    }
+
+    public void tentego() throws SocketException, UnknownHostException, IOException, ParseException {
+        DatagramSocket socket = new DatagramSocket(5001, InetAddress.getByName("127.0.0.1"));
+        System.out.println("Listen on " + socket.getLocalAddress() + " from " + socket.getInetAddress() + " port " + socket.getPort());
+        byte[] buf = new byte[512];
+        DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
+        lastMinute.start(TimeStatistics.getCurrentDate());
+        lastHour.start(TimeStatistics.getCurrentDate());
+        lastDay.start(TimeStatistics.getCurrentDate());
+
+        long timer = System.currentTimeMillis();
+        while (true) {
+            socket.receive(packet);
+            // Data is in format:
+            // <xxx>mmm dd hh:mm:ss hostname user: message
+            String data = new String(packet.getData(), 0, packet.getLength());
+            data = data.substring(data.indexOf('>') + 1);
+            String[] tokens = data.split(" ", 6);
+//            System.out.println(tokens[0]);
+//            System.out.println(tokens[1]);
+//            System.out.println(tokens[2]);
+//            System.out.println(tokens[3]);
+//            System.out.println(tokens[4]);
+//            System.out.println(tokens[5]);
+            System.out.println(data);
+
+            String date = String.format("%s %s %s", tokens[0], tokens[1], tokens[2]);
+            lastMinute.report(date);
+            lastHour.report(date);
+            lastDay.report(date);
+
+            if (System.currentTimeMillis() - timer > 10000) {
+                timer = System.currentTimeMillis();
+                String currentTime = TimeStatistics.getCurrentDate();
+                System.out.println("Number of logs in last minute: " + lastMinute.getSum(currentTime));
+                System.out.println("Number of logs in last hour: " + lastHour.getSum(currentTime));
+                System.out.println("Number of logs in last day: " + lastDay.getSum(currentTime));
+            }
+
+            if (tokens[5].equals("finish\n")) {
+                socket.close();
+                break;
+            }
+
+            KeyedMessage<Integer, String> msg = new KeyedMessage<>("kaflogtopic", data);
+            producer.send(msg);
+        }
+    }
+}
---
+++ Kaflog/src/main/java/pl/edu/agh/kaflog/producer/TimeStatistics.java
@@ -0,0 +1,77 @@
+package pl.edu.agh.kaflog.producer;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class TimeStatistics {
+    public static final SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd HH:mm:ss", Locale.US);
+
+    private int resolution;
+    private int width;
+
+    private int[] cells;
+    private int lastPosition;
+    private int sum;
+
+    private Date startDate;
+
+    public TimeStatistics(int resolution, int width) {
+        this.resolution = resolution;
+        this.width = width;
+
+        cells = new int[width];
+        lastPosition = 0;
+    }
+
+    public static String getCurrentDate() {
+        return dateFormat.format(Calendar.getInstance().getTime());
+    }
+
+    public void start(String dateString) throws ParseException {
+        startDate = dateFormat.parse(dateString);
+    }
+
+    public void report(String dateString) throws ParseException {
+        Date date = dateFormat.parse(dateString);
+        long diff = date.getTime() - startDate.getTime();
+        int seconds = (int) TimeUnit.MILLISECONDS.toSeconds(diff);
+        int position = (seconds / resolution);
+
+        // If the log is older than the last one, assume it's as old as last one
+        if (position < lastPosition) {
+            position = lastPosition;
+        }
+
+        clearCells(lastPosition, position);
+        lastPosition = position;
+        cells[position % width]++;
+        sum++;
+    }
+
+    public int getSum(String dateString) throws ParseException {
+        Date date = dateFormat.parse(dateString);
+        long diff = date.getTime() - startDate.getTime();
+        int seconds = (int) TimeUnit.MILLISECONDS.toSeconds(diff);
+        int position = (seconds / resolution);
+
+        // If the log is older than the last one, assume it's as old as last one
+        if (position < lastPosition) {
+            position = lastPosition;
+        }
+
+        clearCells(lastPosition, position);
+        lastPosition = position;
+        return sum;
+    }
+
+    private void clearCells(int lastPos, int newPos) {
+        for (int i = lastPos + 1; i <= newPos; i++) {
+            sum -= cells[i % width];
+            cells[i % width] = 0;
+        }
+    }
+}
